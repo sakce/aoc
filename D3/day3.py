e0586c9503cdfc69f9f9354c8722122422792be9
sakce <sakovic.fx@gmail.com>
2020-12-03 10:02:08 +0100

    Cleaning

--- D3/day3.py
+++ D3/day3.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+from typing import List
 
 f = open("input.txt", "r")
 forest = f.readlines()
 
+# multiply the strings by 73 so we cover all the combinations of steps (2nd star)
 forest_array = np.array([line.strip("\n")*73 for line in forest])
 
-from typing import List
 
 def find_path(forest, steps: List[int]):
     x = 0
@@ -16,24 +16,29 @@ def find_path(forest, steps: List[int]):
     tree_count = 0
 
     while y < 322:
+
         x += x_step
         y += y_step
-        print(forest[y][x])
+
         if forest[y][x] == "#":
             tree_count += 1
 
     return tree_count
 
-# print(forest_array[1])
+
+print(find_path(forest_array, [3, 1]))
+
 
 def find_best(forest):
-    combs = [[1,1], [3, 1], [5, 1], [7,1], [1, 2]]
+    combs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
     res = []
     for comb in combs:
         res.append(find_path(forest, comb))
 
     print(res)
+
     return np.prod(res)
+
 
 print(find_best(forest_array))
